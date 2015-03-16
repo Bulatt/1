cc4e63ea6dabccfd191efe2e8cfc624f8811c5c6
Bulat Abyanov <boolmc@yandex.ru>
2015-03-17 00:53:32 +0300

    Update css

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -45,48 +45,103 @@ var slideWidth=1266;
     var visibleSlide = 3;
     var blockMargin = 10;
 
-    function makeSlider (a) {
+
+    function makeSlider () {
         var el;
-        $el = $(a);
+        $el = $('#carousel1');
+        var elBlock = $el.find('.blocks');
         $(function(){
-        $el.find('.blocks').width($el.find('.blocks').children().size()*carWidth);
+        elBlock.width(elBlock.children().size()*carWidth);
 
 
-            $('.next_car').click(function(){
+            $('#carousel1 .next_car').click(function(){
                 nextCar();
             });
-            $('.prev_car').click(function(){
+             $('#carousel1 .prev_car').click(function(){
                 prevCar();
             });
         });
 
 
         function nextCar(){
+            var el;
+            $el = $('#carousel1')
             var currentSlide=parseInt($el.find('.blocks').data('current'));
             currentSlide++;
             if(currentSlide>=visibleSlide)
             {
-                $el.find('.blocks').css('left',-(currentSlide-2)*carWidth+blockMargin);
-                $el.find('.blocks').append($el.find('.blocks').children().first().clone());
-                $el.find('.blocks').children().first().remove();
+                elBlock.css('left',-(currentSlide-2)*carWidth);
+                elBlock.append(elBlock.children().first().clone());
+                elBlock.children().first().remove();
                 currentSlide--;
             }
             $el.find('.blocks').animate({left: -currentSlide*carWidth},300).data('current',currentSlide);
         }
 
         function prevCar(){
+            var el;
+            $el = $('#carousel1')
             var currentSlide=parseInt($el.find('.blocks').data('current'));
             currentSlide--;
             if(currentSlide<0)
             {
-                $el.find('.blocks').css('left',-(currentSlide+2)*carWidth-blockMargin);
-                $el.find('.blocks').prepend($el.find('.blocks').children().last().clone());
-                $el.find('.blocks').children().last().remove();
+                elBlock.css('left',-(currentSlide+2)*carWidth);
+                elBlock.prepend($el.find('.blocks').children().last().clone());
+                elBlock.children().last().remove();
                 currentSlide++;
             }
             $el.find('.blocks').animate({left: -currentSlide*carWidth},300).data('current',currentSlide);
         }
     }
-     makeSlider('#carousel1');
-    makeSlider('#carousel2');
+
+    function makeSlider2 () {
+        var el;
+        $el = $('#carousel2');
+        var elBlock = $el.find('.blocks');
+        $(function(){
+        elBlock.width(elBlock.children().size()*carWidth);
+
+
+            $('#carousel2 .next_car').click(function(){
+                nextCar2();
+            });
+            $('#carousel2 .prev_car').click(function(){
+                prevCar2();
+            });
+        });
+
+
+        function nextCar2(){
+            var el;
+            $el = $('#carousel2');
+            var currentSlide=parseInt($el.find('.blocks').data('current'));
+            currentSlide++;
+            if(currentSlide>=visibleSlide)
+            {
+                elBlock.css('left',-(currentSlide-2)*carWidth);
+                elBlock.append(elBlock.children().first().clone());
+                elBlock.children().first().remove();
+                currentSlide--;
+            }
+            $el.find('.blocks').animate({left: -currentSlide*carWidth},300).data('current',currentSlide);
+        }
+
+        function prevCar2(){
+            var el;
+            $el = $('#carousel2');
+            var currentSlide=parseInt($el.find('.blocks').data('current'));
+            currentSlide--;
+            if(currentSlide<0)
+            {
+                elBlock.css('left',-(currentSlide+2)*carWidth);
+                elBlock.prepend($el.find('.blocks').children().last().clone());
+                elBlock.children().last().remove();
+                currentSlide++;
+            }
+            $el.find('.blocks').animate({left: -currentSlide*carWidth},300).data('current',currentSlide);
+        }
+    }
+      makeSlider();
+     // makeSlider2('#carousel2');
+      makeSlider2();
 });
